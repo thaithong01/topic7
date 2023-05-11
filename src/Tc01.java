e22e7223286b2a8d3472a76416d29d14c4cc7c79
thaithong01 <48975735+thaithong01@users.noreply.github.com>
2023-05-12 00:22:27 +0700

    TC 7 Required

--- src/Tc01.java
+++ src/Tc01.java
@@ -1,6 +1,4 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
@@ -70,7 +68,7 @@ public class Tc01 {
         SelectCountry.selectByVisibleText("Canada");
     }
 
-    @Test
+//    @Test
     public void customDropDown(){
         driver.get("https://jqueryui.com/resources/demos/selectmenu/default.html");
         driver.findElement(By.xpath("//span[@id='speed-button']")).click();
@@ -83,9 +81,9 @@ public class Tc01 {
             }
         }
 
-        sleepSecond(3);
+//        sleepSecond(3);
         Assert.assertEquals(driver.findElement(By.xpath("//span[@id='speed-button']//span[@class='ui-selectmenu-text']")).getText(),"Medium");
-        sleepSecond(2);
+//        sleepSecond(2);
 
         driver.findElement(By.xpath("//span[@id='files-button']")).click();
         sleepSecond(3);
@@ -96,11 +94,57 @@ public class Tc01 {
                 break;
             }
         }
-        sleepSecond(3);
+//        sleepSecond(3);
         Assert.assertEquals(driver.findElement(By.xpath("//span[@id='files-button']//span[@class='ui-selectmenu-text']")).getText(),"ui.jQuery.js");
+//        sleepSecond(2);
+    }
+
+//    @Test
+    public void ReactJS(){
+        driver.get("https://react.semantic-ui.com/maximize/dropdown-example-selection/");
+        driver.findElement(By.xpath("//div[@role='listbox']")).click();
         sleepSecond(2);
 
+        List<WebElement> ListPerson = driver.findElements(By.xpath("//div[@role='option']"));
+        for (WebElement itemReactJS: ListPerson){
+            if (itemReactJS.getText().equals("Matt")){
+                itemReactJS.click();
+                break;
+            }
+        }
+        Assert.assertEquals(driver.findElement(By.xpath("//div[@class='divider text']")).getText(),"Matt");
+    }
 
+//    @Test
+    public void VueJS(){
+        driver.get("https://mikerodham.github.io/vue-dropdowns/");
+        driver.findElement(By.xpath("//div[@class='btn-group']")).click();
+        sleepSecond(2);
+        List<WebElement> ListVueJS = driver.findElements(By.xpath("//ul[@class='dropdown-menu']//li//a"));
+        for (WebElement itemVueJS: ListVueJS){
+            if (itemVueJS.getText().equals("First Option")){
+                itemVueJS.click();
+                break;
+            }
+        }
+        Assert.assertEquals(driver.findElement(By.xpath("//li[@class='dropdown-toggle']")).getText(),"First Option");
+    }
+
+    @Test
+    public void editable(){
+        driver.get("https://indrimuska.github.io/jquery-editable-select/");
+        driver.findElement(By.xpath("//div[@id='default-place']//input")).click();
+        sleepSecond(2);
+        List<WebElement> ListEditable = driver.findElements(By.xpath("//div[@id='default-place']//li"));
+        for (WebElement itemEditable: ListEditable){
+            if (itemEditable.getText().equals("Mini")){
+                itemEditable.click();
+                break;
+            }
+        }
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        String actualTest = js.executeScript("return document.querySelector(\"div#default-place input.form-control.es-input\").value;").toString();
+        Assert.assertEquals(actualTest,"Mini");
     }
 
     @AfterTest
